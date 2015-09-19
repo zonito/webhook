f49b781d2baf6673588df9cb74e1416955ed0755
Love Sharma <contact@lovesharma.com>
2015-09-19 14:03:15 +0800

    bitbucket added

--- services.go
+++ services.go
@@ -2,25 +2,26 @@ package webhook
 
 import (
   "encoding/json"
+  "strings"
+  "net/http"
 )
 
 
-/***
-  * Return type of hook.
-  */
+// Return type of hook.
 func getHookType(request *http.Request) string {
   if request.Header.Get("X-Github-Event") != "" {
     return "github"
   } else if (request.Header.Get("X-Sender") == "Doorbell") {
     return "doorbell"
+  } else if (
+      strings.Index(request.Header.Get("User-Agent"), "Bitbucket") > -1) {
+    return "bitbucket"
   }
   return ""
 }
 
 
-/***
-  * Prepare and return description for service.
-  */
+// Prepare and return description for service.
 func getGitDescription(payload GitPayload) string {
   repo := payload.Repository
   desc := repo.Name + "\n===========" +
@@ -41,9 +42,7 @@ func getGitDescription(payload GitPayload) string {
 }
 
 
-/***
-  * Return github data.
-  */
+// Return github data.
 func getGithubData(decoder *json.Decoder, header string) (string, string) {
   var gEvent GitPayload
   decoder.Decode(&gEvent)
@@ -53,9 +52,7 @@ func getGithubData(decoder *json.Decoder, header string) (string, string) {
 }
 
 
-/***
-  * Return doorbell data.
-  */
+// Return doorbell data.
 func getDoorbellData(decoder *json.Decoder) (string, string) {
   var dEvent DBPayload
   decoder.Decode(&dEvent)
@@ -65,4 +62,26 @@ func getDoorbellData(decoder *json.Decoder) (string, string) {
   desc := data.Message + "\n\n **User Agent**: " +
     data.User_Agent + "\n\n **Reply**: " + data.Url
   return event, desc
+}
+
+
+// Return bitbucket data.
+func getBitbucketData(decoder *json.Decoder) (string, string) {
+  var bEvent BBPayload
+  decoder.Decode(&bEvent)
+  if &bEvent.Push != nil {
+    event := bEvent.Repository.Name + ": Push Event "
+    var desc string
+    if len(bEvent.Push.Changes) > 0 {
+      change := bEvent.Push.Changes[0]
+      desc = "\nCommits\n-------"
+      for i := 0; i < len(change.Commits); i++  {
+        desc += "\n\n* " + change.Commits[i].Message +
+          " (" + change.Commits[i].Hash + ")" +
+          "\n * " + change.Commits[i].Author.Raw
+      }
+    }
+    return event, desc
+  }
+  return "", ""
 }
