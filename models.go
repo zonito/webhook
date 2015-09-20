07cf7e29241b3f5af0fe5415c4399387040523c2
Love Sharma <contact@lovesharma.com>
2015-09-21 07:52:14 +0800

    tab to space

--- models.go
+++ models.go
@@ -3,17 +3,17 @@ package webhook
 import "time"
 
 type AccessTokens struct {
-	Email       string
-	AccessToken string // Access token of Trello
+    Email       string
+    AccessToken string // Access token of Trello
 }
 
 type Webhook struct {
-	Handler   string    `json:"handler"`
-	User      string    `json:"-"`
-	BoardId   string    `json:"board_id"`
-	BoardName string    `json:"board_name"`
-	ListId    string    `json:"list_id"`
-	ListName  string    `json:"list_name"`
-	Date      time.Time `json:"date"`
-	Count     int       `json:"count"`
+    Handler   string    `json:"handler"`
+    User      string    `json:"-"`
+    BoardId   string    `json:"board_id"`
+    BoardName string    `json:"board_name"`
+    ListId    string    `json:"list_id"`
+    ListName  string    `json:"list_name"`
+    Date      time.Time `json:"date"`
+    Count     int       `json:"count"`
 }
