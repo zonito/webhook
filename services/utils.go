3c501a3fdf875fa2ae3b6bf75bb5d2e9bca68067
Love Sharma <contact@lovesharma.com>
2015-09-28 00:04:48 +0800

    bitbucket complete

--- services/utils.go
+++ services/utils.go
@@ -53,7 +53,7 @@ func GetEventData(request *http.Request) (string, string) {
     case "doorbell":
         return getDoorbellData(decoder)
     case "bitbucket":
-        return getBitbucketData(decoder)
+        return getBitbucketData(decoder, request.Header.Get("X-Event-Key"))
     case "travis":
         return getTravisData(decoder)
     }
@@ -116,24 +116,70 @@ func getDoorbellData(decoder *json.Decoder) (string, string) {
 }
 
 // Return bitbucket data.
-func getBitbucketData(decoder *json.Decoder) (string, string) {
+func getBitbucketData(decoder *json.Decoder, eType string) (string, string) {
     var bEvent BBPayload
     decoder.Decode(&bEvent)
-    if &bEvent.Push != nil {
-        event := bEvent.Repository.Name + ": Push Event "
-        var desc string
-        if len(bEvent.Push.Changes) > 0 {
-            change := bEvent.Push.Changes[0]
-            desc = "\nCommits\n-------"
-            for i := 0; i < len(change.Commits); i++ {
-                desc += "\n\n* " + change.Commits[i].Message +
-                    " (" + change.Commits[i].Hash + ")" +
-                    "\n * " + change.Commits[i].Author.Raw
+    action := strings.Split(eType, ":")
+    event, desc := "", ""
+    who := bEvent.Actor.Username + " (" + bEvent.Actor.Display_name + ")"
+    if action[0] == "repo" {
+        switch action[1] {
+        case "push":
+            event = bEvent.Repository.Name + ": Push Event"
+            if len(bEvent.Push.Changes) > 0 {
+                change := bEvent.Push.Changes[0]
+                desc = "Who: " + who + "\nCommits\n-------"
+                for i := 0; i < len(change.Commits); i++ {
+                    desc += "\n* " + change.Commits[i].Message +
+                        " (" + change.Commits[i].Hash + ")" +
+                        "\n * " + change.Commits[i].Author.Raw
+                }
             }
+        case "fork":
+            event = bEvent.Repository.Name + ": Fork Event"
+            desc = "\n" + who + " Forked."
+        case "commit_comment_created":
+            event = bEvent.Repository.Name + ": Commit Comment Created"
+            desc = who + " commented on " + bEvent.Commit.Hash
+            desc += "\n Comment: " + bEvent.Comment.Content.Markup
+            desc += "\n File: " + bEvent.Comment.Inline.Path
         }
-        return event, desc
+    } else if action[0] == "pullrequest" {
+        desc = "Description: " + bEvent.Pullrequest.Description
+        desc += "\n From Repository: " + bEvent.Pullrequest.Source.Repository.Name
+        switch action[1] {
+        case "created":
+            desc += "\n Created by: " + who
+        case "updated":
+            desc += "\n Updated by: " + who
+        case "approved":
+            desc += "\n Approved by: " + bEvent.Approval.User.Username
+        case "unapproved":
+            desc += "\n Unapproved by: " + bEvent.Approval.User.Username
+        case "fulfilled":
+            desc += "\n Merged by: " + who
+            desc += "\n **Merged**"
+        case "rejected":
+            desc += "\n Rejected by: " + who
+            desc += "\n **Rejected** because " + bEvent.Pullrequest.Reason
+        case "comment_created":
+            desc += "\n Commented by: " + who
+            desc += "\n Comment: " + bEvent.Comment.Content.Markup
+            desc += "\n File: " + bEvent.Comment.Inline.Path + " at line " +
+                strconv.Itoa(bEvent.Comment.Inline.To)
+        case "comment_updated":
+            desc += "\n Comment updated by: " + who
+            desc += "\n File: " + bEvent.Comment.Inline.Path + " at line " +
+                strconv.Itoa(bEvent.Comment.Inline.To)
+        case "comment_deleted":
+            desc += "\n Comment deleted by: " + who
+            desc += "\n File: " + bEvent.Comment.Inline.Path + " at line " +
+                strconv.Itoa(bEvent.Comment.Inline.To)
+        }
+        event = bEvent.Repository.Name + ": Pull request " + action[1] + ": " +
+            bEvent.Pullrequest.Title + "(" + bEvent.Pullrequest.State + ")"
     }
-    return "", ""
+    return event, desc
 }
 
 // Return travis data.
