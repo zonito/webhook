76d4bcf8a710ac2d33228444bacb91a7eb23f05e
Love Sharma <contact@lovesharma.com>
2015-09-29 23:11:19 +0800

    only git

--- services/git.go
+++ services/git.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+    "appengine"
     "encoding/json"
 )
 
@@ -34,52 +35,71 @@ type GitCommit struct {
 }
 
 type GitUser struct {
-    Login string
-    Id int
+    Login      string
+    Id         int
     Avatar_url string
-    Type string
+    Type       string
     Site_admin bool
 }
 
 type GitPullRequest struct {
-    Url string
-    Id int
-    State string
-    Title string
-    User GitUser
-    Body string
-    Repo GitRepository
+    Url      string
+    Html_url string
+    Id       int
+    State    string
+    Title    string
+    User     GitUser
+    Body     string
+    Repo     GitRepository
 }
 
 type GitPayload struct {
-    Ref        string
-    Compare    string
-    Repository GitRepository
-    Commits    []GitCommit
-    Action string
-    Number int
+    Zen          string
+    Ref          string
+    Compare      string
+    Repository   GitRepository
+    Commits      []GitCommit
+    Action       string
+    Number       int
     Pull_request GitPullRequest
+    Pusher       User
 }
 
 // Return github data.
 func getGithubData(decoder *json.Decoder, header string) (string, string) {
     var gEvent GitPayload
     decoder.Decode(&gEvent)
-    event := gEvent.Repository.Name + " --> " + header + " event"
-    repo := gEvent.Repository
-    desc := repo.Name + ": \n" +
-        "\nName: " + repo.Name +
-        "\nUrl: " + repo.Url +
-        "\nOwner: " + repo.Owner.Email +
-        "\nCompare: " + gEvent.Compare +
-        "\nRef: " + gEvent.Ref +
-        "\nModified files\n"
-    for i := 0; i < len(gEvent.Commits); i++ {
-        commit := gEvent.Commits[i]
-        desc += "\n* " + commit.Message + " (" + commit.Timestamp + ")"
-        for j := 0; j < len(commit.Modified); j++ {
-            desc += "\n * " + commit.Modified[j]
+    var event, desc string
+    if header == "push" {
+        event = gEvent.Repository.Name + " --> " + header + " event"
+        repo := gEvent.Repository
+        desc = repo.Name + ": \n" +
+            "\nName: " + repo.Name +
+            "\nUrl: " + repo.Url +
+            "\nOwner: " + repo.Owner.Email +
+            "\nCompare: " + gEvent.Compare +
+            "\nRef: " + gEvent.Ref +
+            "\nModified files\n"
+        for i := 0; i < len(gEvent.Commits); i++ {
+            commit := gEvent.Commits[i]
+            desc += "\n* " + commit.Message + " (" + commit.Timestamp + ")"
+            for j := 0; j < len(commit.Modified); j++ {
+                desc += "\n * " + commit.Modified[j]
+            }
         }
+    } else if header == "pull_request" {
+        pr := gEvent.Pull_request
+        if gEvent.Action == "opened" {
+            event = "New pull request for " + gEvent.Repository.Full_name +
+                " from " + pr.User.Login
+        }
+        desc = "Title: " + pr.Title
+        if pr.Body != "" {
+            desc += "\nDescription: " + pr.Body
+        }
+        desc += "\nReview at " + pr.Html_url
+    } else if gEvent.Zen != "" {
+        event = "Ping! from " + gEvent.Repository.Name
     }
     return event, desc
 }
