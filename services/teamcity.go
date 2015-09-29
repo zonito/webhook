01bbd2838169327d5b61ec61e5b228787fe2230c
Love Sharma <contact@lovesharma.com>
2015-09-29 23:17:54 +0800

    teamcity

--- services/teamcity.go
+++ services/teamcity.go
@@ -1,40 +1,40 @@
 package services
 
 import (
-  "encoding/json"
+    "encoding/json"
 )
 
 type TCBuild struct {
-  BuildStatus string
-  BuildResult string
-  BuildResultPrevious string
-  BuildResultDetalta string
-  NotifyType string
-  BuildFullName string
-  BuildName string
-  BuildId string
-  BuildTypeId string
-  BuildInternalTypeId string
-  BuildExternalTypeId string
-  BuildStatusUrl string
-  BuildStatusHtml string
-  RootUrl string
-  ProjectName string
-  ProjectId string
-  ProjectInternalId string
-  ProjectExternalId string
-  BuildNumber string
-  AgentName string
-  AgentOs string
-  AgentHostname string
-  TriggeredBy string
-  Message string
-  Text string
-  BuildStateDescription string
+    BuildStatus           string
+    BuildResult           string
+    BuildResultPrevious   string
+    BuildResultDetalta    string
+    NotifyType            string
+    BuildFullName         string
+    BuildName             string
+    BuildId               string
+    BuildTypeId           string
+    BuildInternalTypeId   string
+    BuildExternalTypeId   string
+    BuildStatusUrl        string
+    BuildStatusHtml       string
+    RootUrl               string
+    ProjectName           string
+    ProjectId             string
+    ProjectInternalId     string
+    ProjectExternalId     string
+    BuildNumber           string
+    AgentName             string
+    AgentOs               string
+    AgentHostname         string
+    TriggeredBy           string
+    Message               string
+    Text                  string
+    BuildStateDescription string
 }
 
 type TCPayload struct {
-  Build TCBuild
+    Build TCBuild
 }
 
 // Return teamcity data.
